4424c2dcd0d9c9cb50d7b9211a82377f2b2889d6
chenyouwei <chenyouwei@qupeiyin.com>
2020-09-17 18:55:27 +0800

    feat:新增参考drag video

--- app/src/main/java/com/hugh/byteadvance/MainActivity.java
+++ app/src/main/java/com/hugh/byteadvance/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 import com.hugh.byteadvance.aop.AopActivity;
 import com.hugh.byteadvance.binder.ClientActivity;
+import com.hugh.byteadvance.dragvideo.DragVideoViewActivity;
 import com.hugh.byteadvance.jetpack.databing.DataBindingActivity;
 import com.hugh.byteadvance.jetpack.viewmodel.ViewModelActivity;
 import com.hugh.byteadvance.jetpack.viewmodel.fragment.MainFragActivity;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.go_to_dataBinding).setOnClickListener(this);
         findViewById(R.id.go_to_view_model).setOnClickListener(this);
         findViewById(R.id.go_to_fragment).setOnClickListener(this);
+        findViewById(R.id.go_to_pic_pic).setOnClickListener(this);
     }
 
     @Override
@@ -48,6 +50,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.go_to_fragment:
                 startActivity(new Intent(mActivity, MainFragActivity.class));
+                break;
+            case R.id.go_to_pic_pic:
+                startActivity(new Intent(mActivity, DragVideoViewActivity.class));
                 break;
         }
     }
